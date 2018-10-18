5ce0d9e039b82654285e589d6a6f58505def5ff7
Jack Shao <jacky.shaoxg@gmail.com>
2018-10-18 12:37:29 +0800

    refactor: improve readability for the version dependence of librespeaker

--- scripts/checkinstall.sh
+++ scripts/checkinstall.sh
@@ -10,10 +10,10 @@ cp -f ../scripts/description-pak .
 cp -f ../scripts/postinstall-pak .
 
 version=$(grep "RESPEAKERD_VERSION" ../CMakeLists.txt | sed -e 's/SET(RESPEAKERD_VERSION\ \(.*\))/\1/')
-version_librespeaker=$(grep "SET(RESPEAKER_SDK_VERSION" ../CMakeLists.txt | sed -e 's/SET(RESPEAKER_SDK_VERSION\ \(.*\))/\1/')
+min_librespeaker_version=$(grep "SET(MIN_RESPEAKER_SDK_VERSION" ../CMakeLists.txt | sed -e 's/SET(MIN_RESPEAKER_SDK_VERSION\ \(.*\))/\1/')
 
 echo "version: $version"
-echo "version_librespeaker: ${version_librespeaker}"
+echo "min_librespeaker_version: ${min_librespeaker_version}"
 
 d=$(date +%y%m%d)
 
@@ -30,7 +30,7 @@ checkinstall \
     --pkgversion="$version" \
     --pkgrelease="build$d" \
     --pkgname=respeakerd \
-    --requires="python-mraa,python-upm,libmraa1,libupm1,mraa-tools,libdbus-1-3,pulseaudio,mpg123,mpv,gstreamer1.0-plugins-good,gstreamer1.0-plugins-bad,gstreamer1.0-plugins-ugly,gir1.2-gstreamer-1.0,python-gi,python-gst-1.0,python-pyaudio,librespeaker \(\>= ${version_librespeaker}\)" \
+    --requires="python-mraa,python-upm,libmraa1,libupm1,mraa-tools,libdbus-1-3,pulseaudio,mpg123,mpv,gstreamer1.0-plugins-good,gstreamer1.0-plugins-bad,gstreamer1.0-plugins-ugly,gir1.2-gstreamer-1.0,python-gi,python-gst-1.0,python-pyaudio,librespeaker \(\>= ${min_librespeaker_version}\)" \
     --exclude=/home \
     make install
 
